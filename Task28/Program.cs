2fa2ec320005adb013376d2dcb26d6d825eed77f
Konstantin <>
2022-10-11 22:30:26 +0300

    3 lesson, Task 30

--- Task28/Program.cs
+++ Task28/Program.cs
@@ -4,18 +4,45 @@
 // 4 -> 24
 // 5 -> 120
 
-System.Console.WriteLine("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
 
-int DigitNumbers(int n)
+// int factorial = Factorial(number);
+// Console.WriteLine($"Произведение чисел от 1 до {number} -> {factorial} ");
+
+// int Factorial(int num)
+// {
+//     int fact = 1;
+//     for (int i = 1; i <= num; i++)
+//     {
+//         fact = fact * i;
+//     }
+//     return fact;
+// }
+
+
+
+
+long Factorial(long num)
 {
-    int average = 1;
-    for (int i = 1; i <= n; i++)
+    long fact = 1;
+    int i;
+    for (i = 1; i <= num; i++)
     {
-        average = average *i;
-    
+        try
+        {
+            checked
+            {
+                fact = fact * i;
+            }
+        }
+        catch (Exception)
+        {
+            System.Console.WriteLine("Переполнение типа!");
+            break;
+        }
     }
-    return average;
+
+    System.Console.WriteLine($"Факториал числа {i - 1} = {fact}");
+    return fact;
 }
-int digitNumbers = DigitNumbers(number);
-System.Console.WriteLine($"Количество цифр равно -> {digitNumbers}");
