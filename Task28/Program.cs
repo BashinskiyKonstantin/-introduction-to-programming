2d9f74a571effcdc75542a3b98e7ffa0e874e3f8
Konstantin <>
2022-10-11 21:46:54 +0300

    3 lesson, Task 28

---
+++ Task28/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int DigitNumbers(int n)
+{
+    int average = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        average = average *i;
+    
+    }
+    return average;
+}
+int digitNumbers = DigitNumbers(number);
+System.Console.WriteLine($"Количество цифр равно -> {digitNumbers}");
