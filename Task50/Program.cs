6b42e1ead24d0f94f0dca401c27f55f15c2372e7
Konstantin <>
2022-11-09 22:05:26 +0300

    Task50

---
+++ Task50/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции
+// элемента в двумерном массиве, и возвращает значение этого элемента
+// или же указание, что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 1, 7 -> такого элемента в массиве нет
+
+Console.Write("Введите номер строки элемента: ");
+int rowsElements = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите номер стобца элемента: ");
+int columnsElements = Convert.ToInt32(Console.ReadLine());
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    var matrix = new int[rows, columns];
+    var rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+bool SearchElements(int rows, int columns, int[,] matrix)
+{
+    bool result = default;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (rows == i && columns == j) result = true;
+
+        }
+
+    }
+    return result;
+}
+
+int[,] array2D = CreateMatrixRndInt(3, 4, 1, 10);
+PrintMatrix(array2D);
+bool search = SearchElements(rowsElements, columnsElements, array2D);
+if (search) Console.WriteLine($"Значение элемента {array2D[rowsElements, columnsElements]} ");
+else Console.WriteLine($"{rowsElements}, {columnsElements} - такого элемента в массиве нет");
