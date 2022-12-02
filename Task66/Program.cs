ae2c5f49464147525faec824ee82ba1b0d72be0b
Konstantin <>
2022-12-03 02:51:37 +0300

    9seminar

---
+++ Task66/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 66: Задайте значения M и N.
+// Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите минимальное число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int SumNumbers(int num1, int num2)
+{
+    if (num1 == num2) return num2 + 0;
+    return num2 + SumNumbers(num1, num2 - 1);
+}
+
+if (num1 < num2) Console.WriteLine($"Cумма натуральных элементов в промежутке от {num1} до {num2} -> {SumNumbers(num1, num2)}");
+else Console.WriteLine($"Cумма натуральных элементов в промежутке от {num1} до {num2} -> {SumNumbers(num2, num1)}");
---
+++ Task68/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана
+// с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.Write("Введите первое неотрицательное число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе неотрицательное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int FunctionAkkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0) return FunctionAkkerman(m - 1, 1);
+    return FunctionAkkerman(m - 1, FunctionAkkerman(m, n - 1));
+}
+
+if (n >= 0 && m >= 0) Console.WriteLine($"А({m},{n}) = {FunctionAkkerman(m, n)}");
+else Console.WriteLine("Введены отрицательные числа!");
